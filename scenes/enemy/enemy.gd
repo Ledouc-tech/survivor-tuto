ee8fdb2bb5a65b1f8175ed167338c0c3dc1cbae6
Jules L <lanciaux.jules@yahoo.com>
2026-02-11 11:08:08 +0100

    Initial commit

---
+++ scenes/enemy/enemy.gd
@@ -0,0 +1,59 @@
+extends CharacterBody2D
+
+
+@export var mouvement_speed = 20.0
+@export var hp = 10
+@export var knockback_recovery = 3.5
+@export var experience = 1
+@export var enemy_damage = 1
+var knockback = Vector2.ZERO 
+ 
+@onready var player = get_tree().get_first_node_in_group("player")
+@onready var loot_base = get_tree().get_first_node_in_group("loot")
+@onready var sprite = $Sprite2D
+@onready var anim = $AnimationPlayer
+@onready var snd_hit = $snd_hit
+@onready var hitBox = $hitbox
+
+
+var death_anim = preload("res://scenes/enemy/explosion.tscn")
+var exp_gem = preload("res://objects/experience_gem.tscn")
+
+signal remove_from_array(object)
+
+func _ready():
+	anim.play("walk")
+	hitBox.damage = enemy_damage
+
+func _physics_process(_delta):
+	knockback = knockback.move_toward(Vector2.ZERO, knockback_recovery)
+	var direction = global_position.direction_to(player.global_position)
+	velocity= direction*mouvement_speed
+	velocity += knockback 
+	move_and_slide()
+
+	if direction.x > 0.1 :
+		sprite.flip_h = true
+	elif direction.x < 0.1:
+		sprite.flip_h = false
+		
+func death():
+	emit_signal("remove_from_array",self)
+	var ennemy_death = death_anim.instantiate()
+	ennemy_death.scale = sprite.scale*4
+	ennemy_death.global_position = global_position 
+	get_parent().call_deferred("add_child",ennemy_death)
+	var new_gem = exp_gem.instantiate()
+	new_gem.global_position = global_position
+	new_gem.experience = experience
+	loot_base.call_deferred("add_child", new_gem)
+	queue_free()
+
+
+func _on_hurtbox_hurt(damage, angle, knockback_amount):
+	hp -= damage 
+	knockback = angle * knockback_amount 
+	if hp <= 0:
+		death()
+	else:
+		snd_hit.play()
---
+++ scenes/titl screen/menu.gd
@@ -0,0 +1,12 @@
+extends Control
+
+var level = "res://scenes/world/world.tscn"
+
+
+func _on_btn_play_click_end() -> void:
+	var _level = get_tree().change_scene_to_file(level)
+
+
+func _on_btn_exit_click_end() -> void:
+	get_tree().quit()
+	
